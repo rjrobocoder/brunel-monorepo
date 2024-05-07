1d155b7a0c151c76f3400984e8f6c85683ca9462
rjrobocoder <rjrobocoder@gmail.com>
2024-05-08 00:31:46 +0530

    added page layout

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
+      {/* px-[32px] */}
+      <main className="pb-[32px]">{children}</main>
+      <footer className="px-[32px] pb-[32px]">
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
