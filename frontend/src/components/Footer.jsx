9234bcbd625f27ea74d19658f6ea626a4726a870
rjrobocoder <rjrobocoder@gmail.com>
2024-05-08 15:26:28 +0530

    added dynamic copyright year

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import copyrightIcon from "../assets/copyright-icon.svg";
+import { Link } from "react-router-dom";
+
+const footer_navitems = [
+  {
+    title: "Terms & Conditions",
+    href: "#",
+  },
+  {
+    title: "Privacy Policy",
+    href: "#",
+  },
+];
 
 const Footer = () => {
-    const footer_navitems = [
-        {
-            title: "Terms & Conditions",
-            href: "#",
-        },
-        {
-            title: "Privacy Policy",
-            href: "#",
-        }
-    ];
+  const currentYear = new Date().getFullYear();
 
   return (
     <div className="flex flex-row justify-between items-center rounded-[40px] py-[60px] px-[48px] bg-[#F5F5F5]">
@@ -19,15 +22,17 @@ const Footer = () => {
         <div className="flex gap-x-[10px]">
           <img src={copyrightIcon} alt="©" />
           <p className="text-[18px] text-[#1C1C1C] font-medium">
-            Talup 2023. All rights reserved
+            Talup {currentYear}. All rights reserved
           </p>
         </div>
       </div>
       <div>
         <ul className="flex text-[18px] text-[#1C1C1C] font-normal underline gap-x-[52px]">
-        {/* TODO: apply map */}
-          <li>Terms & Conditions</li>
-          <li>Privacy Policy</li>
+          {footer_navitems?.map((item, index) => (
+            <Link to={item?.href} key={index}>
+              <li>{item?.title}</li>
+            </Link>
+          ))}
         </ul>
       </div>
     </div>
